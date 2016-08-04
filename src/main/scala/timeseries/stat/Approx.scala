b4ca717d2613939092dda2a41657aceee106d3b3
wuciawe <wuciawe@gmail.com>
2016-08-05 07:41:43 +0800

    modify adf halfway

---
+++ src/main/scala/timeseries/stat/Approx.scala
@@ -0,0 +1,11 @@
+package timeseries.stat
+
+/**
+  * Created by jwhu on 8/4/16.
+  */
+object Approx {
+
+  def approx(x: Vector[Double], y: Vector[Double], xout: Double, method: String = "linear", n: Int = 50, yLeft: Double = Double.NaN, yRight: Double = Double.NaN, rule: Int = 1) = {
+
+  }
+}
--- src/main/scala/timeseries/stat/AugumentedDickeyFuller.scala
+++ src/main/scala/timeseries/stat/AugumentedDickeyFuller.scala
@@ -1,23 +1,37 @@
 package timeseries.stat
 
 import org.apache.commons.math3.linear.MatrixUtils
+//import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression
 import regression.RidgeRegression
 import timeseries.util.Difference
 
-object ArgumentedDickeyFuller {
+object AugumentedDickeyFuller {
 
-  private[this] val PVALUE_THRESHOLD = -3.45 //todo: should use a table to compute the p-value
+  private[this] val table = Vector(
+    Vector(-4.38, -3.95, -3.6, -3.24, -1.14, -.8, -.5, -.15),
+    Vector(-4.15, -3.8, -3.5, -3.18, -1.19, -.87, -.58, -.24),
+    Vector(-4.04, -3.73, -3.45, -3.15, -1.22, -.9, -.62, -.28),
+    Vector(-3.99, -3.69, -3.43, -3.13, -1.23, -.92, -.64, -.31),
+    Vector(-3.98, -3.68, -3.42, -3.13, -1.24, -.93, -.65, -.32),
+    Vector(-3.96, -3.66, -3.41, -3.12, -1.25, -.94, -.66, -.33)
+  )
+
+  private[this] val tablen = table(0).length
+  private[this] val tableT = Vector[Double](25, 50, 100, 250, 500, 1e5)
+  private[this] val tablep = Vector(.01, .025, .5, .1, .9, .95, .975, .99)
 
   def test(data: Vector[Double]): ADFResult = test(data, math.cbrt(data.length - 1).floor.toInt)
 
   def test(data: Vector[Double], lag: Int): ADFResult = {
+    require(lag >= 0)
+
     val y = Difference.diff(data, 1)
     val k = lag + 1
-    val n = data.length - 1
+    val n = y.length
     val z = MatrixUtils.createRealMatrix(embed(y, k)) //has rows length(ts) - 1 - k + 1
     val zcol1 = z.getColumnVector(0) //has length length(ts) - 1 - k + 1
     val xt1 = data.slice(k - 1, n)  //ts[k:(length(ts) - 1)], has length length(ts) - 1 - k + 1
-    val trend = 0 to (n - k) //trend k:n, has length length(ts) - 1 - k + 1
+    val trend = (k - 1) until n //trend k:n, has length length(ts) - 1 - k + 1
     val designMatrix =
       if(k > 1) MatrixUtils.createRealMatrix(data.length - 1 - k + 1, 3 + k - 1) // build design matrix as cbind(xt1, 1, trend, yt1)
       else MatrixUtils.createRealMatrix(data.length - 1 - k + 1, 3) // build design matrix as cbind(xt1, 1, tt)
@@ -29,11 +43,12 @@ object ArgumentedDickeyFuller {
       designMatrix.setSubMatrix(yt1.getData, 0, 3)
     }
 
-//    OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
-//    regression.setNoIntercept(true);
-//    regression.newSampleData(zcol1.toArray(), designMatrix.getData());
-//    double[] beta = regression.estimateRegressionParameters();
-//    double[] sd = regression.estimateRegressionParametersStandardErrors();
+//    val regression = new OLSMultipleLinearRegression()
+//    regression.setNoIntercept(true)
+//    regression.newSampleData(zcol1.toArray, designMatrix.getData)
+//    val beta = regression.estimateRegressionParameters()
+//    val sd = regression.estimateRegressionParametersStandardErrors()
+
     val regression = new RidgeRegression(designMatrix.getData.map(_.toVector), zcol1.toArray)
     regression.update(.0001)
     val summary = regression.summary.get
